31e648e9ca4b2f72782f6716889ef20ec71d95e5
Daniel Peinado <daniel.peinadob@gmail.com>
2022-02-23 16:02:36 -0500

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,46 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-        <nav className='navbar'>
+        <nav className='navbar bg-dark'>
             <div className='container'>
                 <a href="/" className='navbar-brand'>
                     <span className='brand-text'>nombre</span>
                 </a>
                 <div className='nav-container'>
-                    <div className='nav-item'>login</div>
-                    <div className='nav-item'>cart</div>
+                    <div className='me-auto'></div>
+                    <div className='nav-upperline'>
+                        <div className='nav-item'>login</div>
+                        <input placeholder='Search your product...' autoComplete='off' />
+                        <div className='nav-item'>cart</div>
+                        <div className='nav-item'>
+                            <a href="/">
+                                <img src="" alt="" />
+                                <span>cart</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
+        <div className='nav-scroller bg-dark'>
+            <nav className='container nav nav-underline'>
+                <li className='nav-item'>
+                    <a href="/" className='nav-link'>products</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/" className='nav-link'>dishes</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/" className='nav-link'>cooking sets</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/" className='nav-link'>kitchen</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/" className='nav-link'>custom designs</a>
+                </li>
+            </nav>
+        </div>
     </>
   )
 }
